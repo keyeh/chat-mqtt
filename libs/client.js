35698cd0a2a7c3c09cb4ecc97749d86eba4b1053
benjamin <benjamin@justpark.com>
2015-07-09 20:18:44 +0100

    Add production capability with connection to mqtt broker

--- libs/client.js
+++ libs/client.js
@@ -1,5 +1,24 @@
 import mqtt from 'mqtt';
+import {mqttConnection} from '../config/settings.json';
 
-let client = mqtt.connect('mqtt://localhost:3000');
+const opts = mqttConnection;
+
+let environment = process.env.NODE_ENV || 'development';
+let client;
+
+if (process.env.NODE_ENV === 'development') {
+  client = mqtt.connect('mqtt://localhost:3000');
+}
+
+if (process.env.NODE_ENV === 'production') {
+  client = mqtt.connect({
+    host: opts.host,
+    port: opts.port,
+    username: opts.username,
+    password: opts.password,
+    protocol: 'wss'
+  });
+}
+
 
 export default client;
